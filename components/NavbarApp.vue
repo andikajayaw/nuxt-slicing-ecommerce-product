df9cf77769fcbc6278cdb99d8bc76f3002459813
LAPTOP-E5999C64\SiNDATA - ANDIKA <andikajayawiguna@gmail.com>
2022-10-25 15:25:42 +0800

    feat: create navbar

---
+++ components/NavbarApp.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar toggleable="lg" variant="bg-transparent text-black">
+    <b-navbar-nav class="mr-auto">
+      <b-nav-item>M-STYLE</b-nav-item>
+    </b-navbar-nav>
+    <b-navbar-nav class="mx-auto order-0">
+      <b-nav-item>SHOP ALL</b-nav-item>
+      <b-nav-item active>MEN</b-nav-item>
+      <b-nav-item>WOMEN </b-nav-item>
+      <b-nav-item>TEENS</b-nav-item>
+    </b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="#">Home</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.navbar.navbar-dark.bg-dark {
+  background-color: #aabb55 !important;
+}
+</style>
