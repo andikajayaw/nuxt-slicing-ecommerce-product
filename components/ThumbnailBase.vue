277d44ee4af483d828f9bc885b659e38ced79a0b
LAPTOP-E5999C64\SiNDATA - ANDIKA <andikajayawiguna@gmail.com>
2022-10-25 15:50:23 +0800

    feat: add assets and create thumbnail

--- components/ThumbnailBase.vue
+++ components/ThumbnailBase.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>Thumbnail</div>
+  <div>
+    <b-nav vertical>
+      <b-nav-item active>
+        <b-img
+          thumbnail
+          fluid
+          src="https://picsum.photos/250/250/?image=53"
+          alt="Image 1"
+        ></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-img
+          thumbnail
+          fluid
+          src="https://picsum.photos/250/250/?image=52"
+          alt="Image 1"
+        ></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-img
+          thumbnail
+          fluid
+          src="https://picsum.photos/250/250/?image=51"
+          alt="Image 1"
+        ></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-img
+          thumbnail
+          fluid
+          src="https://picsum.photos/250/250/?image=54"
+          alt="Image 1"
+        ></b-img>
+      </b-nav-item>
+      <b-nav-item>
+        <b-img
+          thumbnail
+          fluid
+          src="https://picsum.photos/250/250/?image=55"
+          alt="Image 1"
+        ></b-img>
+      </b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
